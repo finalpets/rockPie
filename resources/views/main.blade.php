1095ab67f5c29f03a8b1ea4d795ef49b4dce7e6d
Peter Leyva <peter.leyva.bazan@gmail.com>
2016-11-15 18:04:02 -0800

    Playing with Music using Vue.js

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -2,12 +2,27 @@
 @section('title','| Home Page')
 
 @section('content')
-<h1>Hello World</h1>
-<button class="btn btn-primary">Button</button>
+<div id="app">    
+    <h1>@{{ program_name }}</h1>
+    <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+<input type="text" name="search" placeholder="Enter the Music">
+<hr>
 
-<div id="app">
-    <h2>@{{ message }}</h2>
+
+<audio id="player">
+ <source :src="music"
+         type='audio/mp4'>
+ <!-- The next two lines are only executed if the browser doesn't support MP4 files -->
+ <!-- The next line will only be executed if the browser doesn't support the <audio> tag-->
+ <p>Your user agent does not support the HTML5 Audio element.</p>
+</audio>
+
+<button class="btn btn-success" v-on:click="playMusic">PLAY</button>
 </div>
+<hr>
+
+
+
 @endsection
 
 
@@ -16,7 +31,13 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    program_name: 'RockPie',
+    music: '{{ asset('music/05 - Tifa no Theme (Piano Version).mp3') }}'
+  },
+  methods: {
+    playMusic: function () {
+        document.getElementById('player').play();
+    }
   }
 })
 </script>
