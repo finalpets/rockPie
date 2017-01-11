9a47db0515e2f7ef6cdebfbcfd3eb500c131090e
Peter Leyva <peter.leyva.bazan@gmail.com>
2017-01-11 15:53:55 -0800

    add Style.css to _head.blade.php

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -17,3 +17,12 @@ sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossor
 		<!-- Plugins -->
 		<!-- <link href="css/animations.css" rel="stylesheet"> -->
 		<link rel="stylesheet" type="text/css" href="{{ asset('css/animations.css') }}">
+
+		<link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+		
+		<script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script>
+		<script type='text/javascript' src='plugins/unitegallery/js/unitegallery.min.js'></script> 
+		<link rel='stylesheet' href='plugins/unitegallery/css/unite-gallery.css' type='text/css' /> 
+		<script type='text/javascript' src='plugins/unitegallery/themes/default/ug-theme-default.js'></script> 
+		<link rel='stylesheet' href='plugins/unitegallery/themes/default/ug-theme-default.css' type='text/css' /> 
+		
