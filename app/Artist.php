9c8b3b1b894c33bce2af18e4ee0a674f04f6452b
Peter Leyva <peter.leyva.bazan@gmail.com>
2017-01-20 18:58:59 -0800

    * adding fillable elements

--- app/Artist.php
+++ app/Artist.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Artist extends Model
 {    
+	protected $fillable = ['artist_name'];
+
     public function letters()
     {
     	return $this->belongsTo('App\Letter');
