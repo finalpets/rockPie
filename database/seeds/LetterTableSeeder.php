3a69f35056086325ce49c5c4d862d17a58e9eee0
Peter Leyva <peter.leyva.bazan@gmail.com>
2017-01-20 18:38:47 -0800

    +add LetterTableSeeder for manually generate ABCDEFG

---
+++ database/seeds/LetterTableSeeder.php
@@ -0,0 +1,144 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class LetterTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $letter = new \App\Letter([
+		'letter' => 'A'
+		]);
+        $letter->save();
+
+        $letter = new \App\Letter([
+		'letter' => 'B'
+		]);
+        $letter->save();
+
+        $letter = new \App\Letter([
+		'letter' => 'C'
+		]);
+        $letter->save();
+
+        $letter = new \App\Letter([
+		'letter' => 'D'
+		]);
+        $letter->save();
+
+        $letter = new \App\Letter([
+		'letter' => 'E'
+		]);
+        $letter->save();
+
+        $letter = new \App\Letter([
+		'letter' => 'F'
+		]);
+        $letter->save();
+
+        $letter = new \App\Letter([
+		'letter' => 'G'
+		]);
+        $letter->save();
+
+        $letter = new \App\Letter([
+		'letter' => 'H'
+		]);
+        $letter->save();
+
+        $letter = new \App\Letter([
+		'letter' => 'I'
+		]);
+        $letter->save();
+
+        $letter = new \App\Letter([
+		'letter' => 'J'
+		]);
+        $letter->save();
+
+        $letter = new \App\Letter([
+		'letter' => 'K'
+		]);
+        $letter->save();
+
+        $letter = new \App\Letter([
+		'letter' => 'L'
+		]);
+        $letter->save();
+
+        $letter = new \App\Letter([
+		'letter' => 'M'
+		]);
+        $letter->save();
+
+        $letter = new \App\Letter([
+		'letter' => 'N'
+		]);
+        $letter->save();
+
+        $letter = new \App\Letter([
+		'letter' => 'O'
+		]);
+        $letter->save();
+
+        $letter = new \App\Letter([
+		'letter' => 'P'
+		]);
+        $letter->save();
+
+        $letter = new \App\Letter([
+		'letter' => 'Q'
+		]);
+        $letter->save();
+
+        $letter = new \App\Letter([
+		'letter' => 'R'
+		]);
+        $letter->save();
+
+        $letter = new \App\Letter([
+		'letter' => 'S'
+		]);
+        $letter->save();
+
+        $letter = new \App\Letter([
+		'letter' => 'T'
+		]);
+        $letter->save();
+
+        $letter = new \App\Letter([
+		'letter' => 'U'
+		]);
+        $letter->save();
+
+        $letter = new \App\Letter([
+		'letter' => 'V'
+		]);
+        $letter->save();
+
+        $letter = new \App\Letter([
+		'letter' => 'W'
+		]);
+        $letter->save();
+
+        $letter = new \App\Letter([
+		'letter' => 'X'
+		]);
+        $letter->save();
+
+        $letter = new \App\Letter([
+		'letter' => 'Y'
+		]);
+        $letter->save();
+
+        $letter = new \App\Letter([
+		'letter' => 'Z'
+		]);
+        $letter->save();
+    }
+}
