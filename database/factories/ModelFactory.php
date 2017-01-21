8d4d203322b93e3c88b25890f685cc97262184f6
Peter Leyva <peter.leyva.bazan@gmail.com>
2017-01-20 18:28:25 -0800

    * Add factory Album, to generate fake random info in albums table

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,14 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Album::class, function (Faker\Generator $faker) {    
+
+    return [
+        'album_name' => $faker->name,
+        'year' => $faker->numberBetween(1970,2016),
+        'genre_id' => $faker->numberBetween(1,10),
+        'img' => $faker->domainName,
+		'artist_id' => $faker->numberBetween(1,10),
+    ];
+});
