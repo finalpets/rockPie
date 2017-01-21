009c41f9bb25f73530cbd7edfa49ee12f1c3745f
Peter Leyva <peter.leyva.bazan@gmail.com>
2017-01-20 18:37:26 -0800

    * adding fillable elements

--- app/Letter.php
+++ app/Letter.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Letter extends Model
 {
+	protected $fillable = ['letter'];
+	
     public function artists()
     {
     	return $this->hasMany('App\Artist');
