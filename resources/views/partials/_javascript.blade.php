aaef82eedac548ef67e44fe4bba58076632dcd1d
Peter Leyva <pets.leyva.baza@gmail.com>
2017-02-19 22:17:15 -0800

    *adding virtualkeboard in the js script

--- resources/views/partials/_javascript.blade.php
+++ resources/views/partials/_javascript.blade.php
@@ -14,6 +14,32 @@
 
 
 		<script type="text/javascript">
+
+		// 		var opts = {
+		//   lines: 13 // The number of lines to draw
+		// , length: 28 // The length of each line
+		// , width: 14 // The line thickness
+		// , radius: 42 // The radius of the inner circle
+		// , scale: 1 // Scales overall size of the spinner
+		// , corners: 1 // Corner roundness (0..1)
+		// , color: '#000' // #rgb or #rrggbb or array of colors
+		// , opacity: 0.25 // Opacity of the lines
+		// , rotate: 0 // The rotation offset
+		// , direction: 1 // 1: clockwise, -1: counterclockwise
+		// , speed: 1 // Rounds per second
+		// , trail: 60 // Afterglow percentage
+		// , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+		// , zIndex: 2e9 // The z-index (defaults to 2000000000)
+		// , className: 'spinner' // The CSS class to assign to the spinner
+		// , top: '50%' // Top position relative to parent
+		// , left: '50%' // Left position relative to parent
+		// , shadow: false // Whether to render a shadow
+		// , hwaccel: false // Whether to use hardware acceleration
+		// , position: 'absolute' // Element positioning
+		// }
+		// var target = document.getElementById('myPage')
+		// var spinner = new Spinner(opts).spin(target);
+
 		//var playing = 0;
 		var playSong = "";
 		var isPlaying = false;
@@ -100,6 +126,28 @@
     		}
 
 		    $(document).ready(function(){
+								// *** Hidden input example ***
+				// click on a link - add focus to hidden input
+				$('.hiddenInput').click(function(){
+					$('#hidden').data('keyboard').reveal();
+					return false;
+				});
+				// Initialize keyboard script on hidden input
+				// set "position.of" to the same link as above
+				$('#hidden')
+					.keyboard({
+						layout   : 'qwerty',
+						position : {
+							of : $('.hiddenInput'),
+							my : 'center top',
+							at : 'center top'
+						}
+					})
+					.addTyping();
+
+
+
+
 		    	 $('[data-toggle="tooltip"]').tooltip(); 
 
 				$(function  () {
