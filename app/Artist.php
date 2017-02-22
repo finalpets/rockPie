543813139caffeae4d48d209820490a217d8d0c3
Peter Leyva <peter.leyva.bazan@gmail.com>
2017-02-21 17:31:20 -0800

    * add protected with, for access VIA ajaxRequest to get relatioship attribute

--- app/Artist.php
+++ app/Artist.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Artist extends Model
 {    
-	protected $fillable = ['artist_name'];
-
+	protected $fillable = ['artist_name'];	
+	protected $with = ['Letter'];
     public function letter()
     {    	
     	return $this->belongsTo('App\Letter');
