926fba3d50adf18615994a6d3f15e042558e5548
Peter Leyva <peter.leyva.bazan@gmail.com>
2017-02-21 17:30:22 -0800

    * add protected with, for access VIA ajaxRequest to get relatioship attribute

--- app/Album.php
+++ app/Album.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Album extends Model
 {    
 	protected $fillable = ['album','year','genre_id','img','artist_id'];
+	protected $with = ['Genre','Artist'];
 	public function genre()
 	{		
 		return $this->belongsTo('App\Genre');
