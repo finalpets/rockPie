20dc226c2e8b68dac383ab20e19e4f77a643d779
Peter Leyva <peter.leyva.bazan@gmail.com>
2017-02-24 13:06:55 -0800

    +adding new route for settings and change the name of filemanagerController to update

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,6 @@
 // });
 
 Route::resource('/','AlbumController');
-Route::resource('settings','fileManagerController');
+Route::resource('update','fileManagerController');
 Route::resource('ajaxText','AlbumController');
+Route::get('settings', 'PagesController@getSettings');
